3eb0329a9f2e2e2fd2857497133be4bd83c4f342
asparagusD <nufel1130@gmail.com>
2026-09-04 15:57:50 +0600

    start developing the testbench declaring input and output parameters

---
+++ 8x8 RAM.srcs/sim_1/new/RAM_8_8_tb.v
@@ -0,0 +1,33 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 09/04/2026 03:53:45 PM
+// Design Name: 
+// Module Name: RAM_8_8_tb
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module RAM_8_8_tb(
+
+    );
+    
+    reg clk, rst, wr_enb;
+    reg [3:0] wr_addr;
+    reg [7:0] data_in;
+    reg [3:0] rd_addr;
+    wire [7:0] data_out;
+    
+endmodule
