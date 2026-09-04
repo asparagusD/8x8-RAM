62cc2e06881004e3a1615054f795c3a78a07e66b
asparagusD <nufel1130@gmail.com>
2026-09-04 15:12:59 +0600

    initial commit

---
+++ 8x8 RAM.srcs/sources_1/new/RAM_8_8.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 09/04/2026 03:09:49 PM
+// Design Name: 
+// Module Name: RAM_8_8
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module RAM_8_8(
+
+    );
+endmodule
