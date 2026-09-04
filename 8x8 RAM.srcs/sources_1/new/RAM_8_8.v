91a45dd4031e898ec9152ebbd94447238035f65f
asparagusD <nufel1130@gmail.com>
2026-09-04 15:52:52 +0600

    write the read and write logic of the module

--- 8x8 RAM.srcs/sources_1/new/RAM_8_8.v
+++ 8x8 RAM.srcs/sources_1/new/RAM_8_8.v
@@ -21,12 +21,29 @@
 
 
 module RAM_8_8(
-    input clk, rst, wr_enb, rd_enb,
+    input clk, rst, wr_enb,
     input [3:0] wr_addr, input [7:0] data_in,
+    input [3:0] rd_addr,
     output reg [7:0] data_out
     );
     
     // creating one internal memory
     reg [7:0] mem [7:0];
+    integer i;
+    
+    // write logic and read logic
+    
+    always@(posedge clk or rst)
+        begin
+            if (rst)
+                begin
+                    for (i=0; i<7; i=i+1)
+                        mem[i] <= 0;
+                end
+            else if (wr_enb)
+                mem[wr_addr] <= data_in;
+            else if (wr_enb == 0)
+                data_out <= mem[rd_addr];    
+        end
     
 endmodule
